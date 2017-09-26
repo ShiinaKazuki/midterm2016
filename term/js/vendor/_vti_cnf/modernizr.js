e77ef62f8602fcd4d14e66cc8bbf0757378fcbc3
ShiinaKazuki <hahahaomghi@yahoo.com>
2017-09-26 17:36:18 -0400

    Original

---
+++ term/js/vendor/_vti_cnf/modernizr.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ET\\Instructor
+vti_modifiedby:SR|ET\\student
+vti_timelastmodified:TR|05 Dec 2015 22:30:22 -0000
+vti_timecreated:TR|17 Sep 2015 20:52:51 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Grisaia\\ no\\ Rakuen/default.html master.dwt No\\ Game\\ No\\ Life/default.html Accel\\ World/default.html default.html Sakurasou\\ no\\ Pet\\ na\\ Kanojo/default.html Grisaia\\ No\\ Kaijitsu/default.html Placeholder/Untitled_1.html
+vti_cacheddtm:TX|05 Dec 2015 22:30:22 -0000
+vti_filesize:IR|11272
+vti_syncwith_146.111.120.96\:80/fa15/et_710_fn1/yson91:TX|05 Dec 2015 22:30:21 -0000
+vti_syncofs_146.111.120.96\:80/fa15/et_710_fn1/yson91:TW|10 Dec 2015 21:36:52 -0000
